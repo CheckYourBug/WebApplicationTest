c0d51b6a891b8d56057eb61f352932f1301ae09b
Vladislav Sholokhov <holohov.vladsilav@yandex.ru>
2023-08-24 22:06:22 +0500

    File added

---
+++ 4.1/test_items.py
@@ -0,0 +1,9 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_user_lang(browser):
+    browser.get(link)
+    assert browser.find_elements(By.CSS_SELECTOR, "input+[type='submit']"), 'basket button not found'
