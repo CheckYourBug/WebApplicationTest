8bb8b2b7fd068316e2868cd1223074b91416433f
Vladislav Sholokhov <holohov.vladsilav@yandex.ru>
2023-08-28 22:32:41 +0500

    Add Test of login page

---
+++ test_main_page.py
@@ -0,0 +1,7 @@
+from pages.main_page import MainPage
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_go_to_login_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
