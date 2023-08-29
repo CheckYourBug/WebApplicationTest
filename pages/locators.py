5c90a0227e5ccac53fc255e4c1db31dc15dcec0e
Vladislav Sholokhov <holohov.vladsilav@yandex.ru>
2023-08-29 20:51:44 +0500

    Add locators file which consist of locators pears for project

---
+++ pages/locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+class MainPageLocators():
+    #каждая переменная - пара: (как искать, "что искать (селектор)")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
