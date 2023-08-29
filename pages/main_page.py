315727e3135933317c1250a5ca73a66a7249bae1
Vladislav Sholokhov <holohov.vladsilav@yandex.ru>
2023-08-29 20:31:50 +0500

    modified using method of cathing exceptions + assert message

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,8 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 class MainPage(BasePage):
     #метод даёт ссылку на элемент входа
-    #соответственно называем логично
     #доступ к BasePage и его методм передается через self
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not on the page"
