1b8915c8589a97f59e5ca81a203aefada315183e
Vladislav Sholokhov <holohov.vladsilav@yandex.ru>
2023-08-29 20:52:44 +0500

    modified with getting locators from locators.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,9 @@
 from .base_page import BasePage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 class MainPage(BasePage):
     #метод даёт ссылку на элемент входа
     #доступ к BasePage и его методм передается через self
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not on the page"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not on the page"
