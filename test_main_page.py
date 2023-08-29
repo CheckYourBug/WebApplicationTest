b48547872cde6a2ed69d2032bc79ea2b6cd19e9f
Vladislav Sholokhov <holohov.vladsilav@yandex.ru>
2023-08-29 20:29:29 +0500

    Test of presenting login link button

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,8 @@
 from pages.main_page import MainPage
 link = "http://selenium1py.pythonanywhere.com/"
-
 def test_guest_go_to_login_page(browser):
+    #создаем переменную page которая имеет доступ к классу MainPage, а тот наследник BasePage
+    #имеет доступ к методам обоих классов
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.should_be_login_link()
