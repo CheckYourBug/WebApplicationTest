169e70ff015ad825bb4392a3b5c97462fdd51ed7
Vladislav Sholokhov <holohov.vladsilav@yandex.ru>
2023-09-11 22:04:25 +0500

    new locators added

--- pages/locators.py
+++ pages/locators.py
@@ -2,4 +2,13 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     #каждая переменная - пара: (как искать, "что искать (селектор)")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+class BookPageLocators():
+    BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BOOKNAME = (By.CSS_SELECTOR, ".alertinner>strong")
+    BOOKPRICE = (By.CSS_SELECTOR, ".alertinner>p>strong")
+
+
 
