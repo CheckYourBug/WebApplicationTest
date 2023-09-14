3de187b968bbe1cfe7c7829ad1950fb2b3f66af9
Vladislav Sholokhov <holohov.vladsilav@yandex.ru>
2023-09-14 23:27:03 +0500

    Updated. Added markings

--- test_book_page.py
+++ test_book_page.py
@@ -3,7 +3,8 @@ from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 import pytest
 import faker
-def test_add_to_basket(browser):
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = BookPage(browser, link)
     page.open()
@@ -16,12 +17,13 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = BookPage(browser, link)
     page.open()
     page.should_be_login_link()
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BookPage(browser, link)
     page.open()
     page.go_to_login_page()
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BookPage(browser, link)
@@ -44,6 +46,8 @@ class TestUserAddToBasketFromProductPage():
         page = BookPage(browser, link)
         page.open()
         page.should_not_be_success_message()
+
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = BookPage(browser, link)
